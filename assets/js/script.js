66e9df60fda65f559cc9cb1ed34c494bb8febfbb
EdoRodriguezP <ponce1604@gmail.com>
2025-05-03 12:00:19 -0400

    se suben modificaciones a comentarios

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 
-// Enviar_mensaje
+// Mensaje "enviado correctamente"
 $('#Enviar_mensaje').on("click",function(){
     alert("Tu mensaje fue enviado correctamente... !gracias¡");
 });
